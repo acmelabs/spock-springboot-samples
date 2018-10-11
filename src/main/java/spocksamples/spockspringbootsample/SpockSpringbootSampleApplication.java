ce76f33d2caeff319266bf923e399db24d98fcac
Emre Soylemez and Rafael Tamayo <emre.soylemez+rafael.asensio2@santander.co.uk>
2018-10-11 11:51:46 +0100

    initial commit

---
+++ src/main/java/spocksamples/spockspringbootsample/SpockSpringbootSampleApplication.java
@@ -0,0 +1,12 @@
+package spocksamples.spockspringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockSpringbootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockSpringbootSampleApplication.class, args);
+	}
+}
